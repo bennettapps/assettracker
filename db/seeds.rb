6c95942b09bebb823467e0b8ca600ecc192d7a04
Jesse Bennett <jesse@bennettapps.com>
2023-11-07 07:29:48 +0000

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# 1. Create three categories: Smart Phone, Laptop, and Desktop
-
-# 2. Create the following manufacturers:
-
-# Apple (https://www.apple.com)
-# Lenovo (https://www.lenovo.com)
-# Dell (https://www.dell.com)
-
-# 3. Create 5 numbered employees, e.g. "Employee 1", "Employee 2"
-
-# 4. Create a device for each employee using the first category and first manufacturer. For example,
-# Name: "Smart Phone 1" (use the employee's ID itself for the number)
-# Category: Smart Phone
-# Manufacturer: Apple
-# Employee: Employee 1
-
-# 5. Create a device for each employee using the second category and second manufacturer. For example,
-# Name: "Laptop 1"
-# Category: Laptop
-# Manufacturer: Lenovo
-
-# 6. Create 5 numbered software packages, with a random license_count from 1~5. For example,
-# Name: "Software 1"
-# License count: [a random number from 1 to 5]
-
-# 7. Assign a random software package to each employee.
-# Hint: use a random number for the Software ID when finding it
-
-# 8. Do the above (adding a random software package to each employee) 3 times!
-
 categories = Category.create([
     {name: "Smart Phone"}, 
     {name: "Laptop"}, 
@@ -56,18 +18,16 @@ employees = Employee.create([
     {first_name: "Employee", last_name: "5"}
 ])
 
-devices = Device.create([
-    {name: "Smart Phone " + employees[0].last_name, category: categories[0], manufacturer: manufacturers[0], employee: employees[0]},
-    {name: "Smart Phone " + employees[1].last_name, category: categories[0], manufacturer: manufacturers[0], employee: employees[1]},
-    {name: "Smart Phone " + employees[2].last_name, category: categories[0], manufacturer: manufacturers[0], employee: employees[2]},
-    {name: "Smart Phone " + employees[3].last_name, category: categories[0], manufacturer: manufacturers[0], employee: employees[3]},
-    {name: "Smart Phone " + employees[4].last_name, category: categories[0], manufacturer: manufacturers[0], employee: employees[4]},
-    {name: "Laptop " + employees[0].last_name, category: categories[1], manufacturer: manufacturers[1], employee: employees[0]},
-    {name: "Laptop " + employees[1].last_name, category: categories[1], manufacturer: manufacturers[1], employee: employees[1]},
-    {name: "Laptop " + employees[2].last_name, category: categories[1], manufacturer: manufacturers[1], employee: employees[2]},
-    {name: "Laptop " + employees[3].last_name, category: categories[1], manufacturer: manufacturers[1], employee: employees[3]},
-    {name: "Laptop " + employees[4].last_name, category: categories[1], manufacturer: manufacturers[1], employee: employees[4]}
-])
+devices = []
+categories.each do |category|
+  manufacturers.each do |manufacturer|
+    devices << Device.create(
+      name: "Device " + rand(1..100).to_s,
+      category: category,
+      manufacturer: manufacturer
+    )
+  end
+end
 
 softwares = Software.create([
     {name: "Software 1", license_count: rand(1..5)},
@@ -80,5 +40,9 @@ softwares = Software.create([
 for i in 1..3 do
     for j in 0..4 do
         employees[j].softwares << softwares[rand(0..4)]
+        devices_available = devices.select { |device| device.employee_id.nil? }
+        if devices_available.any?
+            employees[j].devices << devices_available.sample
+        end
     end
 end
