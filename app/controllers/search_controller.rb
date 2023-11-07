6b347ffadb3e2c1edc60842df52b45a7dbb57106
Jesse Bennett <jesse@bennettapps.com>
2023-11-07 06:18:30 +0000

    add category filter

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
   def index
     @query = params[:query]
+    @category_id = params[:category_id]
+
     @devices = Device.where("name like '%#{@query}%'")
+
+    if @category_id.present?
+      @devices = @devices.where(category_id: @category_id)
+    end
   end
 end
